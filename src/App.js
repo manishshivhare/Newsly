93fa06460fd74fb44cfe937e05156c92a04630cc
Manish Shivhare <67515371+manishshivhare@users.noreply.github.com>
2024-09-14 16:30:20 +0530

    UI update

--- src/App.js
+++ src/App.js
@@ -5,24 +5,36 @@ import Header from "./components/Header.jsx";
 
 function App() {
   const [news, setNews] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
   const fetchNews = async () => {
     try {
       const response = await axios.get(
-        `https://gnews.io/api/v4/search?q=example&lang=en&country=india&max=10&apikey=${process.env.REACT_APP_API_KEY}`
+        `https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=${process.env.REACT_APP_API_KEY}`
       );
-      setNews(response.data.articles); // Assuming response contains `articles` array
+      setNews((prevNews) => [...prevNews, ...response.data.articles]);
     } catch (error) {
       console.error("Error fetching news:", error);
+    } finally {
+      setLoading(false);
     }
   };
+
+  const handleLoadMore = async () => {
+    setLoading(true);
+    fetchNews();
+  };
+
   useEffect(() => {
+    setLoading(true);
     fetchNews();
   }, []);
 
   return (
     <div>
       <Header />
-      <div className="container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="container mx-auto p-4 grid grid-cols-1 gap-6 w-[800px]">
         {news.map((article, index) => (
           <Card
             key={index}
@@ -32,8 +44,18 @@ function App() {
             link={article.url}
             sourceName={article.source.name}
             sourceLink={article.source.url}
+            publishedAt={article.publishedAt}
           />
         ))}
+      </div>
+      <div className="text-center py-4">
+        <button
+          onClick={handleLoadMore}
+          disabled={loading}
+          className="px-6 py-3 bg-[#53c1c0] text-white rounded-lg hover:bg-[#45b2a5] focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-offset-2 transition duration-150 ease-in-out text-lg"
+        >
+          {loading ? "Loading..." : "Load More"}
+        </button>
       </div>
     </div>
   );
--- src/components/CardBlock.jsx
+++ src/components/CardBlock.jsx
@@ -1,25 +1,35 @@
+import dayjs from "dayjs";
+
 const Card = (props) => {
+  const date = props.publishedAt;
+  const formattedDate = dayjs(date).format("DD MMM YYYY [at] HH:mm");
+
   return (
-    <div className="relative max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg transition-transform transform hover:scale-105 dark:bg-gray-800 dark:border-gray-700">
-      <a href={props.link} aria-label={props.headline}>
-        <img
-          className="rounded-t-lg w-full h-48 object-cover"
-          src={props.img}
-          alt={props.headline || "News image"}
-        />
-      </a>
-      <div className="p-6">
+    <div className="flex flex-col sm:flex-row bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 transition-transform transform ">
+      {/* Image on top for mobile, on left for larger screens */}
+
+      <img
+        className=" flex-shrink-0 sm:w-48 w-full rounded-t-lg sm:rounded-l-lg sm:rounded-tr-none w-full sm:h-full object-cover h-48"
+        src={props.img}
+        alt={props.headline || "News image"}
+      />
+
+      {/* Content */}
+      <div className="p-6 flex-1">
         <a href={props.link} aria-label={props.headline}>
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-lg sm:text-xl font-bold tracking-tight text-gray-700 dark:text-white">
             {props.headline}
           </h5>
         </a>
-        <p className="mb-4 text-base leading-relaxed text-gray-700 dark:text-gray-400">
+        <div className="text-sm text-gray-400 mb-2">
+          Published on {formattedDate}
+        </div>
+        <p className="mb-4 text-base leading-relaxed text-gray-500 dark:text-gray-400">
           {props.description}
         </p>
         <a
           href={props.link}
-          className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-offset-gray-800"
+          className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-[#53c1c0] rounded-md hover:bg-[#45b2a5] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-offset-gray-800"
         >
           Read more
           <svg
@@ -41,7 +51,10 @@ const Card = (props) => {
       </div>
       <div className="absolute bottom-2 right-2 text-sm text-gray-500 dark:text-gray-400">
         <span>Source-</span>
-        <a href={props.sourceLink} className="ml-1 text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300">
+        <a
+          href={props.sourceLink}
+          className="ml-1 text-[#53c1c0] hover:text-[#45b2a5] dark:text-blue-400 dark:hover:text-blue-300"
+        >
           {props.sourceName}
         </a>
       </div>
