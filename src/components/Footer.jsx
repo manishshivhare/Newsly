21a78fa5c187cede6a23e5a1d1f7832906fe10d6
Manish Shivhare <67515371+manishshivhare@users.noreply.github.com>
2024-09-16 01:21:15 +0530

    Footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,19 +3,43 @@ import companyLogo from "../assets/newslyLogo.png";
 
 const Footer = () => {
   return (
-    <footer className="dark:bg-gray-800 text-white py-8 bg-white border-t border-gray-200 dark:border-gray-700">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div className="mb-4 md:mb-0 flex items-center">
-          <img
-            src={companyLogo}
-            alt="Company Logo"
-            className="h-8 w-auto object-contain mr-2 ml-5 filter dark:invert"
-          />
-        </div>
-      </div>
+    <footer class="bg-white shadow dark:bg-gray-900 border-t dark:border-gray-700">
+      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+          <a
+            href="http://manish-shivhare-newsly.netlify.app/"
+            class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src={companyLogo}
+              class="h-8 filter dark:invert"
+              alt="Flowbite Logo"
+            />
+          </a>
+          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li>
+              <p class="hover:underline me-4 md:me-6">About</p>
+            </li>
+            <li>
+              <p class="hover:underline me-4 md:me-6">Privacy Policy</p>
+            </li>
 
-      <div className="text-center mt-4 text-sm text-gray-400">
-        © 2024 Newsly. All rights reserved.
+            <li>
+              <p class="hover:underline">Contact</p>
+            </li>
+          </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2023{" "}
+          <a
+            href="http://manish-shivhare-newsly.netlify.app/"
+            class="hover:underline"
+          >
+            Newsly™
+          </a>
+          . All Rights Reserved.
+        </span>
       </div>
     </footer>
   );
