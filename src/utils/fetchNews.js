f82f907edc1a1a85d604b8c9fd7009531f005ffa
Manish Shivhare <67515371+manishshivhare@users.noreply.github.com>
2024-09-22 11:53:01 +0530

    api update

--- src/utils/fetchNews.js
+++ src/utils/fetchNews.js
@@ -2,13 +2,15 @@ import axios from "axios";
 import { setLoading, setNextPage } from "../redux/UserSlicer/userSlice";
 
 export const fetchNews = async (dispatch, category, page = 0) => {
+  
   try {
     dispatch(setLoading(true));
     const { data } = await axios.get(
-      `https://newsdata.io/api/1/latest?apikey=${process.env.REACT_APP_API_KEY}&language=en` +
+      `https://newsdata.io/api/1/latest?apikey=${process.env.REACT_APP_API_KEY}&language=en&timezone=Asia/Kolkata&removeduplicate=1 +
         (page ? `&page=${page}` : "") + (category ? `&category=${category}` : "")
     );
     dispatch(setNextPage(data.nextPage));
+    console.log(data.results);
     return data.results || [];
   } catch (error) {
     console.error("Error fetching news:", error);
